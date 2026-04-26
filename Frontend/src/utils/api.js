01517c8a33947645f542c9f6c83242a69e780475
Ankit Kumar <ankit9113456145@gmail.com>
2026-04-26 15:29:10 +0530

    fix frontend api

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL ='http://localhost:5000/api';
+const API_BASE_URL ='import.meta.env.VITE_API_URL;';
 
 const apiCall = async (endpoint, options = {}) => {
   const token = localStorage.getItem('flavorcraft-token');
